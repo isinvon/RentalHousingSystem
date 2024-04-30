ff098e98b8f331a7a34917a617e304d1d87ceb25
林信欢 <2021469084@qq.com>
2024-04-30 18:53:02 +0800

    add: 添加分页组件

---
+++ house/src/components/MyPagnation.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-pagination background layout="prev, pager, next"
+                 :page-size="20"
+                 :pager-count="11"
+                 :total="1000"/>
+</template>
