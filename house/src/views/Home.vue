2fb0c978a9dabc5540ca3449853b65f5cc794450
林信欢 <2021469084@qq.com>
2024-04-30 18:59:57 +0800

    update: 重命名分页组件MyPagnation->MyPagination

--- house/src/views/Home.vue
+++ house/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="common-layout">
-    <el-container>
+    <el-container class="el-container">
       <!-- 左边 -->
       <el-container style="width: auto;">
         <!-- 页头 -->
@@ -10,19 +10,17 @@
         </el-header>
         <!-- 中间 -->
         <el-main>
-
-
-          <MyPagnation style="display: flex"/>
+          <MyPagination style="display: flex"/>
         </el-main>
         <!-- 页脚 -->
         <el-footer>Footer</el-footer>
       </el-container>
 
-      <!-- 右边 -->
-      <el-aside width="200px">
-        <!-- 侧边栏 -->
-        <MySideMenu style="width: auto;"/>
-      </el-aside>
+       <!--右边 -->
+      <!--<el-aside width="200px">-->
+      <!--  &lt;!&ndash; 侧边栏 &ndash;&gt;-->
+      <!--  <MySideMenu style="width: auto;"/>-->
+      <!--</el-aside>-->
     </el-container>
   </div>
 </template>
@@ -30,11 +28,18 @@
 <script setup>
 import MyTopMenu from "@/components/MyTopMenu/index.vue";
 import MySideMenu from "@/components/MySideMenu/index.vue";
-import MyPagnation from "@/components/MyPagnation.vue";
+import MyPagination from "@/components/MyPagination.vue";
 </script>
 
 <style>
+.el-container{
+  display: flex;
+}
 
+MySideMenu{
+  width: 10px;
+  height: 10px;
+}
 
 
 </style>
