cf1a292801ca116ca51425fc4c8c0c53ed8760d8
林信欢 <2021469084@qq.com>
2024-04-30 18:17:37 +0800

    add: 添加Home组件基本布局

---
+++ house/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <!-- 左边 -->
+      <el-container style="width: auto;">
+        <!-- 页头 -->
+        <el-header>
+
+        </el-header>
+        <!-- 中间 -->
+        <el-main>Main</el-main>
+        <!-- 页脚 -->
+        <el-footer>Footer</el-footer>
+      </el-container>
+
+      <!-- 右边 -->
+      <el-aside width="200px">
+
+      </el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+
+
+</style>
