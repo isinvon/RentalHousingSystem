9bf05a6ea2269008b64ee026e808ac8f171ed8bc
林信欢 <2021469084@qq.com>
2024-05-03 22:33:48 +0800

    update 修改顶栏菜单信息

--- house/src/components/MyTopMenu/index.vue
+++ house/src/components/MyTopMenu/index.vue
@@ -1,61 +1,36 @@
 <template>
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <el-menu-item index="1">Processing Center</el-menu-item>
-      <el-sub-menu index="2">
-        <template #title>Workspace</template>
-        <el-menu-item index="2-1">item one</el-menu-item>
-        <el-menu-item index="2-2">item two</el-menu-item>
-        <el-menu-item index="2-3">item three</el-menu-item>
-        <el-sub-menu index="2-4">
-          <template #title>item four</template>
-          <el-menu-item index="2-4-1">item one</el-menu-item>
-          <el-menu-item index="2-4-2">item two</el-menu-item>
-          <el-menu-item index="2-4-3">item three</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="3" disabled>Info</el-menu-item>
-      <el-menu-item index="4">Orders</el-menu-item>
-    </el-menu>
-    <div class="h-6" />
-    <!-- <el-menu
-      :default-active="activeIndex2"
-      class="el-menu-demo"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      @select="handleSelect"
-    >
-      <el-menu-item index="1">Processing Center</el-menu-item>
-      <el-sub-menu index="2">
-        <template #title>Workspace</template>
-        <el-menu-item index="2-1">item one</el-menu-item>
-        <el-menu-item index="2-2">item two</el-menu-item>
-        <el-menu-item index="2-3">item three</el-menu-item>
-        <el-sub-menu index="2-4">
-          <template #title>item four</template>
-          <el-menu-item index="2-4-1">item one</el-menu-item>
-          <el-menu-item index="2-4-2">item two</el-menu-item>
-          <el-menu-item index="2-4-3">item three</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="3" disabled>Info</el-menu-item>
-      <el-menu-item index="4">Orders</el-menu-item>
-    </el-menu> -->
-  </template>
-  
-  <script lang="js" setup>
-  import { ref } from 'vue'
-  
-  const activeIndex = ref('1')
-  const activeIndex2 = ref('1')
-  const handleSelect = (key, keyPath) => {
-    console.log(key, keyPath)
-  }
-  </script>
-  
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <!-- <el-sub-menu index="2"> -->
+    <!-- 可以展开的 菜单 -->
+    <!-- <template #title>Workspace</template>
+<el-menu-item index="2-1">item one</el-menu-item>
+<el-menu-item index="2-2">item two</el-menu-item>
+<el-menu-item index="2-3">item three</el-menu-item>
+<el-sub-menu index="2-4">
+  <template #title>item four</template>
+  <el-menu-item index="2-4-1">item one</el-menu-item>
+  <el-menu-item index="2-4-2">item two</el-menu-item>
+  <el-menu-item index="2-4-3">item three</el-menu-item>
+</el-sub-menu> -->
+    <!-- </el-sub-menu> -->
+    <!-- 禁用的菜单 -->
+    <!-- <el-menu-item index="3" disabled></el-menu-item> -->
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">所有房源</el-menu-item>
+    <el-menu-item index="3">招租文章</el-menu-item>
+    <el-menu-item index="4">求租文章</el-menu-item>
+    <el-menu-item index="4">新闻资讯</el-menu-item>
+  </el-menu>
+  <div class="h-6" />
+
+</template>
+
+<script lang="js" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const activeIndex2 = ref('1')
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+</script>
