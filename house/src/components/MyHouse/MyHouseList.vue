881f989bab3fd5ba97aced9ca64d63da9ae8b018
林信欢 <2021469084@qq.com>
2024-05-05 01:56:48 +0800

    add 添加房源列表

---
+++ house/src/components/MyHouse/MyHouseList.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-row :gutter="20">
+        <el-col v-for="(item, index) in 12" :span="6">
+            <div class="grid-content ep-bg-purple" />
+            <!-- 房源卡片 -->
+            <MyHouseCard />
+        </el-col>
+    </el-row>
+</template>
+
+<script setup>
+import MyHouseCard from '@/components/MyHouseCard.vue'
+</script>
+
+
+<style lang="scss" scoped>
+.el-row {
+    margin-bottom: 20px;
+}
+
+.el-row:last-child {
+    margin-bottom: 0;
+}
+
+.el-col {
+    border-radius: 4px;
+}
+
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+</style>
---
+++ house/src/views/House.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <MyHouseList />
+        <MyPagination />
+    </div>
+</template>
+
+<script setup>
+import MyHouseList from '@/components/MyHouse/MyHouseList.vue'
+import MyPagination from '@/components/MyPagination.vue'
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
