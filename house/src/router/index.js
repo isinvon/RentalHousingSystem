e14c2fd13831a34174f4896dc94b7a991e3067cd
林信欢 <2021469084@qq.com>
2024-05-04 19:54:18 +0800

    add 添加路由

--- house/src/router/index.js
+++ house/src/router/index.js
@@ -1,25 +1,73 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    // 首页
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
+    // 所有房源
+    path: "/house",
+    name: "house",
+    // 重定向
+    redirect: "/house/houseList",
+    // 子路由
+    children: [
+      {
+        path: "houseList",
+        name: "houseList",
+        component: () => import("@/components/MyHouse/MyHouseList.vue"),
+      },
+      {
+        path: "houseDetail",
+        name: "houseDetail",
+        component: () => import("@/components/MyHouse/MyHouseDetail.vue"),
+      },
+    ],
+  },
+  {
+    // 新闻资讯
+    path: "/news",
+    name: "news",
+    // 重定向
+    redirect: "/news/newsList",
+    children: [
+      {
+        path: "newsList",
+        name: "newsList",
+        component: () => import("@/components/News/NewsList.vue"),
+      },
+      {
+        path: "newsDetail",
+        name: "newsDetail",
+        component: () => import("@/components/News/NewsDetail.vue"),
+      },
+    ],
+  },
+  {
+    // 登录
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/index.vue"),
+  },
+  {
+    // 关于我们
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
