ebbd036bbbc5c4ee5d8bfd5c026c6ee245502f79
林信欢 <2021469084@qq.com>
2024-05-26 17:37:29 +0800

    add 添加UserController类

---
+++ server/src/main/java/com/sinvon/server/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.sinvon.server.controller;
+
+import com.sinvon.server.entity.User;
+import com.sinvon.server.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
