56fe52fbf1fcc24a5fa2368e79f6796704b62f18
林信欢 <2021469084@qq.com>
2024-05-26 17:40:23 +0800

    add UserServiceImpl

---
+++ server/src/main/java/com/sinvon/server/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sinvon.server.service.impl;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sinvon.server.entity.User;
+import com.sinvon.server.mapper.UserMapper;
+import com.sinvon.server.service.UserService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+/**
+ * 用户实现层
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+
+
+
+
+    @Override
+    public User selectAll() {
+        // 查询所有用户信息
+        List<User> list = baseMapper.selectList(null);
+        return (User) list;
+    }
+
+    @Override
+    public User login(User user) {
+        return null;
+    }
+
+    @Override
+    public User register(User user) {
+        return null;
+    }
+
+
+    @Override
+    public User forgetPassword(User user) {
+        return null;
+    }
+}
