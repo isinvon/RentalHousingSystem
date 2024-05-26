8e05a0b100adaaa9388aae96bfebb6172f90056a
林信欢 <2021469084@qq.com>
2024-05-26 17:36:21 +0800

    add 启动成功信息

--- server/src/main/java/com/sinvon/server/ServerApplication.java
+++ server/src/main/java/com/sinvon/server/ServerApplication.java
@@ -1,13 +1,25 @@
 package com.sinvon.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+        System.out.println("\n" +
+                "     _______. __  .__   __. ____    ____  ______   .__   __. \n" +
+                "    /       ||  | |  \\ |  | \\   \\  /   / /  __  \\  |  \\ |  | \n" +
+                "   |   (----`|  | |   \\|  |  \\   \\/   / |  |  |  | |   \\|  | \n" +
+                "    \\   \\    |  | |  . `  |   \\      /  |  |  |  | |  . `  | \n" +
+                ".----)   |   |  | |  |\\   |    \\    /   |  `--'  | |  |\\   | \n" +
+                "|_______/    |__| |__| \\__|     \\__/     \\______/  |__| \\__| \n" +
+                "                                                             \n");
+        log.info("项目启动成功...");
     }
 
 }
